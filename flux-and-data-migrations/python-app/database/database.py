308f558511a8ebc64b6bdb17fdd7a5fadba6f31c
Alisson Menezes <Alisson.itops@gmail.com>
2024-08-25 18:08:52 +0200

    FluxCD and Migrations

---
+++ flux-and-data-migrations/python-app/database/database.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Boolean, Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///data/sql_app.db"
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, unique=True, index=True)
+    first_name = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+    # items = relationship("Item", back_populates="owner")
---
+++ flux-and-data-migrations/python-app/main.py
@@ -0,0 +1,17 @@
+from database.database import SessionLocal
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/")
+def index():
+    return {"message": "Hello, I am a python app deployed by FluxCD"}
