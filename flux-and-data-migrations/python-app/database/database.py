5fee7dff589fd60b6c2fe902e4c15e6dd54888aa
Alisson Menezes <Alisson.itops@gmail.com>
2024-08-28 20:26:50 +0200

    adding last_name column on database

--- flux-and-data-migrations/python-app/database/database.py
+++ flux-and-data-migrations/python-app/database/database.py
@@ -18,6 +18,7 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     email = Column(String, unique=True, index=True)
     first_name = Column(String, unique=True, index=True)
+    last_name = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
 
